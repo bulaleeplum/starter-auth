afd508e7474225b59e820f1e367366026ab2c56b
Spenser Lee <Spenser.L159@gmail.com>
2016-04-03 03:14:26 -0700

    added per user menu bar

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -46,14 +46,31 @@ class Application extends CI_Controller {
 	// build menu choices depending on the user role
 	function makemenu()
 	{
-		$choices = array();
+		// get all menu items from config
+		$choices = $this->config->item('menu_choices')['menudata'];
+		$choice = array();
 
-		$choices[] = array('name' => "Alpha", 'link' => '/alpha');
-		$choices[] = array('name' => "Beta", 'link' => '/beta');
-		$choices[] = array('name' => "Gamma", 'link' => '/gamma');
-		$choices[] = array('name' => "Login", 'link' => '/auth');
-		$choices[] = array('name' => "Logout", 'link' => '/auth/logout');
-		return $choices;
+		$role = $this->session->userdata('userRole');
+		$name = $this->session->userdata('userName');
+
+		$choice[] = $choices[0];		// alpha page
+
+		if ($role == null) {
+			$choice[] = $choices[3];	// login
+			return $choice;
+		}
+		if ($role == "user") {
+			$choice[] = $choices[1];	// beta page
+			$choice[] = $choices[4];	// logout
+		}
+		if ($role == "admin") {
+			$choice[] = $choices[1];	// beta page
+			$choice[] = $choices[2];	// gamma page
+			$choice[] = $choices[4];	// logout
+		}
+		$choice[] = array('name' => 'Logged in as ' . $name, 'link' => '');
+
+		return $choice;
 	}
 
 	function restrict($roleNeeded = null) {
